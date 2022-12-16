924e4035bd6bcc76fe6734845cf5ecb19c7d9a92
soumah yaya <soumah_yaya@hotmail.com>
2022-12-16 13:32:46 +0800

    server connection setup completed

---
+++ README.md
@@ -0,0 +1,19 @@
+===========================================================================
+                                MOVIE -REST API
+===========================================================================
+
+
+Most of your work will be implementing methods in the **dao** directory, which
+contains all database interfacing methods. The API will make calls to Data
+Access Objects (DAOs) that interact directly with MongoDB.
+
+**running the server
+npm start
+
+** base url
+http://127.0.0.1:5000/api/v1/private/
+
+
+
+
+
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import app from "./server"
+import { MongoClient } from "mongodb"
+import color from "colors"
+
+
+const port = process.env.PORT || 8080
+
+MongoClient.connect(
+    process.env.MOVIE_URI,
+    {
+        maxPoolSize: 50,
+        waitQueueTimeoutMS: 2500
+
+    },
+    { useNewUrlParser: true },
+    { useUnifiedTopology: true }
+
+)
+    .catch(err => {
+        console.error(err.stack)
+        process.exit(1)
+    })
+    .then(() => {
+        
+        app.listen(port, () => {
+            console.log(`server running at: http://127.0.0.1: ${port}/api/v1/private/`.underline.cyan)
+        })
+    })
